611183e37154d5ae9243c7169176e083997af0ac
Leonardo de Abreu <leonardo.abreu@lia.ufc.br>
2021-01-28 01:15:16 -0300

    added parameters sliders

--- pisim-ui.c
+++ pisim-ui.c
@@ -4,12 +4,23 @@
 #include "pisim.h"
 
 #define PROG_NAME "π Simulador"
+#define NPOINTS 100
+#define MAXPTS 1500
+#define SMULTIP 0x9696969696
+#define SADITIV 0x00800AAAAAAAAAABp1
+#define SMODULO 0x07FFFFFFFFFFFFFFp1
+#define SLIDERSTEPS 30
 
 #define PISIM_TYPE_APP (pisim_app_get_type())
 
 #define PISIM_TYPE_WINDOW (pisim_app_window_get_type())
 #define PISIM_APP_WINDOW(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), PISIM_TYPE_WINDOW, PisimAppWindow))
 #define PISIM_WINDOW_CLASS(class) (G_TYPE_CHECK_CLASS_CAST((class), PISIM_TYPE_WINDOW, PisimAppWindowClass))
+
+const char init_labels[] = "100\0"                       // 0 - 3
+							"165573515712001\0"          // 4 - 19
+							"36040525142993579\0"        // 20 - 37
+							"1152921504606846975\0";     // 38 - 57
 
 typedef struct _data_t {
 	cairo_surface_t * plot;
@@ -21,15 +32,13 @@ typedef struct _data_t {
 
 #define DATA_INIT ((data_t) {NULL, NULL, 0, 0, MULTIP, ADITIV, MODULO})
 
-
-static void draw_graph (GtkDrawingArea * drawing_area, cairo_t * cr,
-                                   int width, int height, gpointer window);
-static void parameter_change(GtkAdjustment * adj, gpointer window);
-
 typedef struct _PisimAppWindow {
 	GtkApplicationWindow parent;
 	GtkWidget * canvas;
 	GtkWidget * size_value;
+	GtkWidget * multip_value;
+	GtkWidget * aditiv_value;
+	GtkWidget * modulo_value;
 	GtkWidget * pi_label;
 	data_t context;
 } PisimAppWindow;
@@ -40,14 +49,35 @@ typedef struct _PisimAppWindowClass {
 
 G_DEFINE_TYPE(PisimAppWindow, pisim_app_window, GTK_TYPE_APPLICATION_WINDOW);
 
+static void draw_graph (GtkDrawingArea * drawing_area, cairo_t * cr,
+                                   int width, int height, gpointer window);
+static void size_change(GtkAdjustment * adj, gpointer window);
+static void multip_change(GtkAdjustment * adj, gpointer window);
+static void aditiv_change(GtkAdjustment * adj, gpointer window);
+static void modulo_change(GtkAdjustment * adj, gpointer window);
+
+static void parameter_update(PisimAppWindow * window);
+
 static void pisim_app_window_init(PisimAppWindow * window) {
 	GtkWidget * content;
 	GtkWidget * draw;
 	GtkWidget * control;
 	GtkWidget * size_label;
+	GtkWidget * multip_label;
+	GtkWidget * aditiv_label;
+	GtkWidget * modulo_label;
 	GtkAdjustment * size_adjust;
+	GtkAdjustment * multip_adjust;
+	GtkAdjustment * aditiv_adjust;
+	GtkAdjustment * modulo_adjust;
 	GtkWidget * size_slider;
 	GtkWidget * size_value;
+	GtkWidget * multip_slider;
+	GtkWidget * multip_value;
+	GtkWidget * aditiv_slider;
+	GtkWidget * aditiv_value;
+	GtkWidget * modulo_slider;
+	GtkWidget * modulo_value;
 	GtkWidget * pi_box;
 	GtkWidget * pi_label;
 	window->context = DATA_INIT;
@@ -67,29 +97,95 @@ static void pisim_app_window_init(PisimAppWindow * window) {
 
 	gtk_box_prepend(GTK_BOX(content), draw);
 
-	control = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-	gtk_box_set_homogeneous(GTK_BOX(control), TRUE);
+	control = gtk_grid_new();
+	gtk_grid_set_column_homogeneous(GTK_GRID(control), TRUE);
 	gtk_box_append(GTK_BOX(content), control);
 
 	size_label = gtk_label_new("Nº de pontos");
 	/* gtk_widget_set_halign(size_label, GTK_ALIGN_END); */
 	gtk_label_set_xalign(GTK_LABEL(size_label), 1.0);
-	gtk_box_append(GTK_BOX(control), size_label);
+	gtk_grid_attach(GTK_GRID(control), size_label, 0, 0, 1, 1);
 
-	size_adjust = gtk_adjustment_new(100, 1, 1500, 100, 100, 0);
-	parameter_change(size_adjust, window);
+	size_adjust = gtk_adjustment_new(NPOINTS, 1, MAXPTS,
+			MAXPTS/SLIDERSTEPS, 10 * MAXPTS/SLIDERSTEPS, 0);
+	size_change(size_adjust, window);
 
 	size_slider = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, size_adjust);
 	gtk_scale_set_digits(GTK_SCALE(size_slider), 0);
 	gtk_scale_set_has_origin(GTK_SCALE(size_slider), TRUE);
-	gtk_box_append(GTK_BOX(control), size_slider);
+	gtk_grid_attach(GTK_GRID(control), size_slider, 1, 0, 1, 1);
 
-	size_value = gtk_label_new("100");
+	size_value = gtk_label_new(init_labels);
 	window->size_value = size_value;
 	gtk_label_set_xalign(GTK_LABEL(size_value), 0.0);
-	gtk_box_append(GTK_BOX(control), size_value);
+	gtk_label_set_selectable(GTK_LABEL(size_value), TRUE);
+	gtk_grid_attach(GTK_GRID(control), size_value, 2, 0, 1, 1);
 
-	g_signal_connect(size_adjust, "value-changed", G_CALLBACK(parameter_change), window);
+	g_signal_connect(size_adjust, "value-changed", G_CALLBACK(size_change), window);
+
+
+	multip_label = gtk_label_new("Multiplicativo");
+	gtk_label_set_xalign(GTK_LABEL(multip_label), 1.0);
+	gtk_grid_attach(GTK_GRID(control), multip_label, 0, 1, 1, 1);
+
+	multip_adjust = gtk_adjustment_new(SMULTIP, 1, 2 * SMULTIP,
+			2 * SMULTIP / SLIDERSTEPS, 10 * 2 * SMULTIP / SLIDERSTEPS, 0);
+	/* multip_change(multip_adjust, window); */
+
+	multip_slider = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, multip_adjust);
+	gtk_scale_set_digits(GTK_SCALE(multip_slider), 0);
+	gtk_scale_set_has_origin(GTK_SCALE(multip_slider), TRUE);
+	gtk_grid_attach(GTK_GRID(control), multip_slider, 1, 1, 1, 1);
+
+	multip_value = gtk_label_new(init_labels + 4);
+	window->multip_value = multip_value;
+	gtk_label_set_xalign(GTK_LABEL(multip_value), 0.0);
+	gtk_label_set_selectable(GTK_LABEL(multip_value), TRUE);
+	gtk_grid_attach(GTK_GRID(control), multip_value, 2, 1, 1, 1);
+
+	g_signal_connect(multip_adjust, "value-changed", G_CALLBACK(multip_change), window);
+
+	aditiv_label = gtk_label_new("Aditivo");
+	gtk_label_set_xalign(GTK_LABEL(aditiv_label), 1.0);
+	gtk_grid_attach(GTK_GRID(control), aditiv_label, 0, 2, 1, 1);
+
+	aditiv_adjust = gtk_adjustment_new(SADITIV, 1, 2 * SADITIV,
+			2 * SADITIV/SLIDERSTEPS, 10 * 2 * SADITIV/SLIDERSTEPS, 0);
+	/* aditiv_change(aditiv_adjust, window); */
+
+	aditiv_slider = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, aditiv_adjust);
+	gtk_scale_set_digits(GTK_SCALE(aditiv_slider), 0);
+	gtk_scale_set_has_origin(GTK_SCALE(aditiv_slider), TRUE);
+	gtk_grid_attach(GTK_GRID(control), aditiv_slider, 1, 2, 1, 1);
+
+	aditiv_value = gtk_label_new(init_labels + 20);
+	window->aditiv_value = aditiv_value;
+	gtk_label_set_xalign(GTK_LABEL(aditiv_value), 0.0);
+	gtk_label_set_selectable(GTK_LABEL(aditiv_value), TRUE);
+	gtk_grid_attach(GTK_GRID(control), aditiv_value, 2, 2, 1, 1);
+
+	g_signal_connect(aditiv_adjust, "value-changed", G_CALLBACK(aditiv_change), window);
+
+	modulo_label = gtk_label_new("Modulo");
+	gtk_label_set_xalign(GTK_LABEL(modulo_label), 1.0);
+	gtk_grid_attach(GTK_GRID(control), modulo_label, 0, 3, 1, 1);
+
+	modulo_adjust = gtk_adjustment_new(SMODULO, 1, 2 * SMODULO,
+			2 * SMODULO/SLIDERSTEPS, 10 * 2 * SMODULO/SLIDERSTEPS, 0);
+	/* modulo_change(modulo_adjust, window); */
+
+	modulo_slider = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, modulo_adjust);
+	gtk_scale_set_digits(GTK_SCALE(modulo_slider), 0);
+	gtk_scale_set_has_origin(GTK_SCALE(modulo_slider), TRUE);
+	gtk_grid_attach(GTK_GRID(control), modulo_slider, 1, 3, 1, 1);
+
+	modulo_value = gtk_label_new(init_labels + 38);
+	window->modulo_value = modulo_value;
+	gtk_label_set_xalign(GTK_LABEL(modulo_value), 0.0);
+	gtk_label_set_selectable(GTK_LABEL(modulo_value), TRUE);
+	gtk_grid_attach(GTK_GRID(control), modulo_value, 2, 3, 1, 1);
+
+	g_signal_connect(modulo_adjust, "value-changed", G_CALLBACK(modulo_change), window);
 
 	pi_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
 	gtk_box_set_homogeneous(GTK_BOX(pi_box), TRUE);
@@ -97,6 +193,7 @@ static void pisim_app_window_init(PisimAppWindow * window) {
 
 	pi_label = gtk_label_new("π ≈ ...");
 	window->pi_label = pi_label;
+	gtk_label_set_selectable(GTK_LABEL(pi_label), TRUE);
 	gtk_box_append(GTK_BOX(pi_box), pi_label);
 }
 
@@ -135,39 +232,85 @@ static void draw_graph (GtkDrawingArea * drawing_area, cairo_t * cr,
 	cairo_mask_surface(cr, context->plot, 0, 0);
 }
 
-static void parameter_change(GtkAdjustment * adj, gpointer window) {
-	double pi;
-	size_t n = gtk_adjustment_get_value(adj);
-	size_t max = gtk_adjustment_get_upper(adj);
-	data_t * context = &((PisimAppWindow *) window)->context;
+static void size_change(GtkAdjustment * adj, gpointer window) {
+	size_t n = gtk_adjustment_get_value(adj),
+		   max = gtk_adjustment_get_upper(adj);
+	char value[8];
 	GtkWidget * size_value = ((PisimAppWindow *) window)->size_value;
-	GtkWidget * pi_label = ((PisimAppWindow *) window)->pi_label;
+	data_t * context = &((PisimAppWindow *) window)->context;
+
 	context->size = n;
+	if (context->_max_size == 0){
+		context->_max_size = max;
+		context->points = malloc(max * sizeof(point_t));
+	}
+
+	snprintf(value, 8, "%zu", n);
+	gtk_label_set_text(GTK_LABEL(size_value), value);
+
+	parameter_update((PisimAppWindow *) window);
+}
+
+static void multip_change(GtkAdjustment * adj, gpointer window) {
+	long a = 1 + 4 * gtk_adjustment_get_value(adj);
+	char value[32];
+	GtkWidget * multip_value = ((PisimAppWindow *) window)->multip_value;
+	data_t * context = &((PisimAppWindow *) window)->context;
+
+	snprintf(value, 32, "%ld", a);
+	gtk_label_set_text(GTK_LABEL(multip_value), value);
+
+	parameter_update((PisimAppWindow *) window);
+}
+
+static void aditiv_change(GtkAdjustment * adj, gpointer window) {
+	long b = gtk_adjustment_get_value(adj);
+	char value[32];
+	GtkWidget * aditiv_value = ((PisimAppWindow *) window)->aditiv_value;
+	data_t * context = &((PisimAppWindow *) window)->context;
+
+	snprintf(value, 32, "%ld", b);
+	gtk_label_set_text(GTK_LABEL(aditiv_value), value);
+
+	parameter_update((PisimAppWindow *) window);
+}
+
+static void modulo_change(GtkAdjustment * adj, gpointer window) {
+	long m = 1 + 2 * gtk_adjustment_get_value(adj);
+	char value[32];
+	GtkWidget * modulo_value = ((PisimAppWindow *) window)->modulo_value;
+	data_t * context = &((PisimAppWindow *) window)->context;
+
+	snprintf(value, 32, "%ld", m);
+	gtk_label_set_text(GTK_LABEL(modulo_value), value);
+
+	parameter_update((PisimAppWindow *) window);
+}
+
+static void parameter_update(PisimAppWindow * window) {
+	double pi;
+	char value[32];
+	GtkWidget * pi_label = window->pi_label;
+	data_t * context = &(window->context);
+	size_t n = context->size,
+		   max = context->_max_size;
 	point_t * p;
 	cairo_t * cr;
-	GtkWidget * draw = ((PisimAppWindow *) window)->canvas;
+	GtkWidget * draw = window->canvas;
 	int width = gtk_widget_get_width(draw),
 		height = gtk_widget_get_height(draw),
 		unit = MIN(height, width)/2.0;
 	GtkNative * native = gtk_widget_get_native(draw);
 
-	char value[32];
-	snprintf(value, 32, "%zu", n);
-	gtk_label_set_text(GTK_LABEL(size_value),
-	                   value);
 
-	if (context->_max_size == 0 && context->_max_size != max){
-		context->_max_size = max;
-		context->points = malloc(max * sizeof(point_t));
-	}
-
-	pi = calcpi(max, context->a, context->b, context->m, context->points);
+	pi = calcpi(context->_max_size, context->a, context->b,
+			    context->m, context->points);
 	snprintf(value, 32, "π ≈ %10.8lf", pi);
 	gtk_label_set_text(GTK_LABEL(pi_label),
 	                   value);
 
 	p = context->points;
-	printf("%lf  %zu  %zu  %lf\n",pi, n, max, p[max-1].x);
+	printf("%lf  %zu  %zu  %lf\n",pi, context->size, context->_max_size, p[context->_max_size-1].x);
 
 	if (context->plot != NULL) cairo_surface_destroy(context->plot);
 	context->plot = cairo_image_surface_create(CAIRO_FORMAT_A8, width, height);
@@ -176,7 +319,7 @@ static void parameter_change(GtkAdjustment * adj, gpointer window) {
 
 	cairo_set_source_rgba(cr, 0, 0, 0, 1.0);
 	cairo_set_line_width(cr, 0);
-	for (size_t i = 0; i < n; i++) {
+	for (size_t i = 0; i < context->size; i++) {
 		cairo_arc(cr, width/2.0 + p[i].x * unit,
 					  height/2.0 + p[i].y * unit,
 					  2.0, 0, 2*G_PI
